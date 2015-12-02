ad79ede32e42ce4c10c0fb48345e045c20d3f3d2
pulli23 <pweijtens@gmail.com>
2015-12-02 14:13:50 +0100

    Build before merge

---
+++ app/src/main/java/com/scoutingstuff/paul/ivossenjacht/DirectionFragment.java
@@ -0,0 +1,20 @@
+package com.scoutingstuff.paul.ivossenjacht;
+
+/**
+ * Created by Paul on 12/1/2015.
+ */
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DirectionFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_main, container, false);
+
+    }
+}
---
+++ app/src/main/java/com/scoutingstuff/paul/ivossenjacht/DrawShape.java
@@ -0,0 +1,7 @@
+package com.scoutingstuff.paul.ivossenjacht;
+
+/**
+ * Created by Paul on 12/1/2015.
+ */
+public class DrawShape {
+}
